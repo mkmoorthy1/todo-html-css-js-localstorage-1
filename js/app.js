49a69971e4838a29d870fd4416611ad073568587
mkmoorthy1 <mkmoorthy1@gmail.com>
2022-07-27 10:14:19 +0530

    todo prj with html-css-js localstorage

--- js/app.js
+++ js/app.js
@@ -1,43 +1,73 @@
-let todosData=JSON.parse(localStorage.getItem('todos'))||[]
+let todosData = JSON.parse(localStorage.getItem('todos')) || [];
 
-const data={
-    todos:todosData,
-    setTodos: function(data){
-        this.todos=data
-    }
+const data = {
+	todos: todosData,
+	setTodos: function(data) {
+		this.todos = data;
+	},
+};
 
+let todoInput = document.getElementById('todo-input');
+let todoBox = document.getElementById('todo-box');
+
+let generateTodos = () => {
+	todoBox.innerHTML = data.todos
+		.map(x => {
+			let { id, todoItem, completed } = x;
+			return `<li><span class="${completed === true
+				? 'completed'
+				: ''}">${todoItem}</span><i onclick="toogledone(${id})" class="fa-solid fa-circle-check"></i><i class="fa-solid fa-trash-can" onclick="deleteTask(${id})"></i></li>`;
+		})
+		.join('');
+};
+
+generateTodos();
+
+function toogledone(id) {
+	console.log(id);
+	let findTodo = data.todos.find(x => x.id === id);
+
+	if (findTodo) {
+		console.log(findTodo);
+		findTodo.completed = !findTodo.completed;
+	}
+
+	data.todos.map(y => (y.id === id ? findTodo : y));
+
+	localStorage.setItem('todos', JSON.stringify(data.todos));
+	generateTodos();
 }
 
-let todoInput=document.getElementById("todo-input");
+function addTodo(event) {
+	event.preventDefault();
 
+	// let todoInput=todoInput.value.trim()
 
-let generateTodos=()=>{
-    
+	if (todoInput.value === '') {
+		alert('Please Enter Todo');
+	}
+
+	const newTodo = {
+		id:data.todos?.length ? data.todos[data.todos.length-1].id+1:1,
+		todoItem: todoInput.value,
+		completed: false,
+	};
+
+	data.setTodos([ ...data.todos, newTodo ]);
+
+	localStorage.setItem('todos', JSON.stringify(data.todos));
+
+	generateTodos();
+
+	todoInput.value = '';
 }
 
+function deleteTask(id) {
+	console.log(id);
 
-function addTodo(event){
-    
-    event.preventDefault();
+	data.todos = data.todos.filter(x => x.id !== id);
 
-    // let todoInput=todoInput.value.trim()
+	generateTodos();
 
-    if(todoInput.value === ""){
-        alert("Please Enter Todo")
-    }
-
-
-    const newTodo={
-        id:data.todos?.length ? data.todos[data.todos.length-1].id+1:1,
-        todoItem:todoInput.value,
-        completed:false
-    }
-
-
-    data.setTodos([...data.todos,newTodo]);
-
-    localStorage.setItem('todos',JSON.stringify(data.todos));
-
-    todoInput.value="";
-
+	localStorage.setItem('todos', JSON.stringify(data.todos));
 }
